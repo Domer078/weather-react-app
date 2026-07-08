ae9e33de475b04c2ec1586a7c7122326924a34c0
Domer078 <khomotsomabala05@gmail.com>
2026-07-08 19:27:55 +0200

    move project files to VS Code workspace

---
+++ src/weather.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+
+export default function Weather(props) {
+  const [temperature, setTemperature] = useState(props.temperature);
+  const [city, setCity] = useState("");
+  const [weatherInfo, setWeatherInfo] = useState("");
+
+  function handleWeather(event) {
+    event.preventDefault();
+    setWeatherInfo(`It is ${temperature}°C in ${city}`);
+  }
+
+  function updateCity(event) {
+    setCity(event.target.value);
+  }
+
+  return (
+    <div>
+      <form onSubmit={handleWeather}>
+        <input type="text" placeholder="Type a city" onChange={updateCity} />
+        <input type="submit" value="search" />
+      </form>
+      <h2>{weatherInfo}</h2>
+    </div>
+  );
+}
