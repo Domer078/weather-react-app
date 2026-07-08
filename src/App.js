4dd10cb81d569540f9bd4e62f3c7e71714642727
Domer078 <khomotsomabala05@gmail.com>
2026-07-08 22:50:48 +0200

    Added target blank attribute to my links

--- src/App.js
+++ src/App.js
@@ -8,9 +8,13 @@ function App() {
       <Weather city="Paris" />
       <p>
         This code was coded with ❤️ and is open-sourced on{" "}
-        <a href="https://github.com/Domer078/weather-react-app">Github</a> and
-        hosted on{" "}
-        <a href="https://magical-panda-4acfd3.netlify.app/">Netlify</a>
+        <a href="https://github.com/Domer078/weather-react-app" target="blank">
+          Github
+        </a>{" "}
+        and hosted on{" "}
+        <a href="https://magical-panda-4acfd3.netlify.app/" target="blank">
+          Netlify
+        </a>
       </p>
     </div>
   );
